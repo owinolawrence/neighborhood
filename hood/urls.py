f34230770f1d1124cbbfabea2274885944ede091
owinolawrence <owinolawrence21@gmail.com>
2020-01-21 16:04:09 +0300

    add profile ,update and add_neighbourhood urls

--- hood/urls.py
+++ hood/urls.py
@@ -11,10 +11,5 @@ urlpatterns = [
     url('profile/',views.profile, name='profile'),
     url('update/',views.update_profile,name='update_profile'),
     url('add_neighbourhood/',views.add_neighbourhood,name='add_neighbourhood'),
-    url('add_business/',views.add_business,name='add_business'),
-    url('business/',views.business,name="business"),
-    url('add_post/',views.add_post,name="add_post"),
-    url(r'^join_neighbourhood/(?P<hood_id>\d+)$',views.join_neighbourhood,name='join'),
-    url(r'^leave_neighbourhood/(?P<hood_id>\d+)$',views.leave_neighbourhood,name='leave'),
-    url(r'^s_neighbourhood/(?P<hood_id>\d+)$',views.s_neighbourhood,name='details'),
+        
 ]
