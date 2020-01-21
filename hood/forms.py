2bff6d9381561585de8734626120eceace693fa9
owinolawrence <owinolawrence21@gmail.com>
2020-01-21 16:11:55 +0300

    create class RegisterForm to display form for users

---
+++ hood/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile,Posts,Neighbourhood,Business
+class RegisterForm(UserCreationForm):
+    fullname = forms.CharField(max_length=150,required=False,help_text='Optional.')
+    
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    
+    class Meta :
+        model = User
+        fields =('username','fullname','email','password1','password2')
+
+
