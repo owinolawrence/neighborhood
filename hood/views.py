dd066e8df76d8b89e262d3b8c96e04fb5741d0d6
owinolawrence <owinolawrence21@gmail.com>
2020-01-21 15:52:18 +0300

    create a profile and profile update view function for users profile

--- hood/views.py
+++ hood/views.py
@@ -30,3 +30,31 @@ def signup(request):
         form = RegisterForm()
     return render (request, 'registration/registration_form.html', {'form':form})
 
+@login_required
+def profile(request):
+    post = Posts.objects.all()
+    current_user = request.user
+    profile = get_object_or_404(Profile, name=current_user)
+    return render(request, 'main/profile.html',{"current_user":current_user,"profile":profile,"post":post})
+
+@login_required
+def update_profile(request):
+  post = Posts.objects.all()
+  if request.method == 'POST':
+    user_form = UpdateUser(request.POST,instance=request.user)
+    profile_form = UpdateProfileForm(request.POST,request.FILES,instance=request.user.profile)
+    if user_form.is_valid() and profile_form.is_valid():
+      user_form.save()
+      profile_form.save()
+      messages.success(request,'Your Profile account has been updated successfully')
+      return redirect('profile')
+  else:
+    user_form = UpdateUser(instance=request.user)
+    profile_form = UpdateProfileForm(instance=request.user.profile) 
+  forms = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'post':post
+  }
+  return render(request,'main/edit_profile.html',forms)
+
